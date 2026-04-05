3d841422e1db9f23b687bde75edb33e1767f9eb5
AzmirAntor <antorazmir7@gmail.com>
2026-04-05 23:30:53 +0600

    Fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-
 import './App.css'
 import Banner from './Components/Banner'
 import Navbar from './Components/Navbar'
 import Stats from './Components/Stats'
 
-function App() {  
+const getProducts = async () => {
+  const res = await fetch("/models.json");
+  console.log(getProducts)
+  return res.json()
+}
+
+const productPromise = getProducts()
+console.log(productPromise)
+function App() {
 
   return (
     <>
-      <Navbar></Navbar>
-      <Banner></Banner>
-      <Stats></Stats>
-      {}
+      <div>
+
+        <Navbar></Navbar>
+
+          <Banner></Banner>
+          <Stats></Stats>
+            
+        
+      </div>
     </>
   )
 }
